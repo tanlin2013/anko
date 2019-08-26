df7a56c03d75ba1401f32cc29090ac9307ad3b27
TaoLinTan <tao-lin.tan@commerce-connector.com>
2019-08-26 13:52:58 +0800

    update to v0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'tamago',
   packages = ['tamago'],
-  version = '0.0.1',
+  version = '0.0.2',
   license='MIT',
   description = 'Toolkit for performing anomaly detection algorithm on time series.',
   author = 'tao-lin',
