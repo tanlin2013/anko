ad44444574aeea29794ec9842eb16f15b3c172d6
TaoLinTan <tao-lin.tan@commerce-connector.com>
2019-09-07 00:19:17 +0800

    update docs

--- docs/html/search/pages_0.js
+++ docs/html/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['anko_85',['anko',['../index.html',1,'']]]
+  ['anko_20_3c_21_26mdash_3b_7b_23mainpage_7d_2d_2d_3e_85',['anko &lt;!&amp;mdash;{#mainpage}--&gt;',['../md__r_e_a_d_m_e.html',1,'']]]
 ];
